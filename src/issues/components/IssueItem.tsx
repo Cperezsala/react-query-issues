cdf43c680ccf54e07222a602e05952b097f86104
Carlos <carlos_dev@slumadrid.slu.edu>
2023-03-14 17:43:52 +0100

    fin seccion 6

--- src/issues/components/IssueItem.tsx
+++ src/issues/components/IssueItem.tsx
@@ -4,6 +4,7 @@ import { FiInfo, FiMessageSquare, FiCheckCircle } from "react-icons/fi";
 import { useNavigate } from "react-router-dom";
 import { getIssueInfo, getIssueComments } from "../hooks/useIssue";
 import { Issue, State } from "../interfaces";
+import { timeSince } from '../../helpers';
 
 interface Props {
   issue: Issue;
@@ -45,9 +46,23 @@ export const IssueItem: FC<Props> = ({ issue }) => {
         <div className="d-flex flex-column flex-fill px-2">
           <span>{issue.title}</span>
           <span className="issue-subinfo">
-            #{issue.number} opened 2 days ago by{" "}
+            #{issue.number} opened{timeSince(issue.created_at)} ago by{" "}
             <span className="fw-bold">{issue.user.login}</span>
           </span>
+          <div>
+            {
+              issue.labels.map((label)=> (
+                <span
+                key={label.id}
+                className="badge rounded-pill m-1"
+                style={{backgroundColor: `#${ label.color}`, color: 'black'}}
+                >
+                  {label.name}
+                  
+                </span>
+              ))
+            }
+          </div>
         </div>
 
         <div className="d-flex align-items-center">
