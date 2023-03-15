86542593a720af7574525c62549c6bdb7a471c5f
Carlos <carlos_dev@slumadrid.slu.edu>
2023-03-15 15:31:30 +0100

    fin seccion 7

--- src/api/githubApi.ts
+++ src/api/githubApi.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 export const githubApi = axios.create({
     baseURL: 'https://api.github.com/repos/facebook/react',
     headers: {
-        Authorization: 'Bearer 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'
+        Authorization: 'Bearer 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'
     }
 })
 
